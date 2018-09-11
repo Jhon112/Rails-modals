f20770cea386f07af7334723d04f68312b69760a
Jhon Arias <jhonarmont@hotmail.com>
2018-09-10 19:20:20 -0500

    Create a button to make an alert

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,8 @@
 class ProductsController < ApplicationController
   def index
   end
+
+  def alert_js
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products
+  root 'products#index'
+  get 'alert_js', to: 'products#alert_js', as: 'alert_js'
 end
