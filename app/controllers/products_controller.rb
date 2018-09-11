8243cb0c48308908a726bc80c5498246fcc6af34
Jhon Arias <jhonarmont@hotmail.com>
2018-09-10 19:29:12 -0500

    Create new and create methods. Added product params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,21 @@ class ProductsController < ApplicationController
   def alert_js
     
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+  end
+
+
+
+  private
+    def product_params 
+      params.requite(:product).permit(:name, :price)
+    end
+
+
 end
