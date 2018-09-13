603f21c02fa551b1a1bf0db10e40d19fa45f3591
Jhon Arias <jhonarmont@hotmail.com>
2018-09-12 20:04:45 -0500

    Create products and test scopes and methods Margen and Stock_status

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,19 @@
 class Product < ApplicationRecord
+    validates :name, presence: true
+
+    scope :published, -> {where(published: true)}
+
+    def word_count
+        name.split(" ").length
+    end
+
+    def margen
+        (price - costo)/costo
+    end
+
+    def stock_status
+        quantity > 0 ? true : false        
+    end
+
     
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @product1 = products(:product1)
+    @product2 = products(:product2)
+    
+  end
+
+  test "Product is not created without name" do
+    product = Product.new
+    assert_equal false, product.save
+  end
+
+  test ".word_count returns the correct number of wors in name" do
+    assert_equal 1, @product1.word_count
+  end
+
+  test "Published scope only returns published Products" do
+    products = Product.published
+    assert_equal 2, products.length
+  end
+
+  # haga un test qu pruebe que el metodo 'margen' calcule correctamente el margen de los productos(agregar el campo costo)
+  test ".margen calculates the margen of the products correctly" do
+    assert_equal 1, @product1.margen
+
+  end
+
+  test ".stock_status returns if a product is available" do
+    assert_equal true, @product1.stock_status
+    assert_equal false, @product2.stock_status
+
+  end
+
+
+
+
+
 end
